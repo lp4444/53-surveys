1724667ae7d4d5e1c3ead886628c14055f7c0873
lp4444 <jack86520t@yahoo.com.tw>
2022-08-21 15:56:13 +0800

    add mongoose

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-// const cookieSession = require("cookie-session");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 
-// const passport = require("passport");
+const passport = require("passport");
 
 // const keys = require("./config/keys");
 // require("./models/User");
@@ -21,20 +21,20 @@ const express = require("express");
 //   .catch((err) => console.log("ee", err, "eee"));
 
 const app = express();
-// app.use(express.json());
-// app.use(
-//   cookieSession({
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieKey],
-//   })
-// );
+app.use(express.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
 // app.use(passport.initialize());
 // app.use(passport.session());
 
 // app.use("/", auth).use("/", survey);
 app.get("/", (req, res) => {
-  res.send("Hello World3!");
+  res.send("Hello World6!");
 });
 
 const PORT = process.env.PORT || 5000;
