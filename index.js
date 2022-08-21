dddb8c9f9dc256753f4fc46721a72254c2aeb8f7
lp4444 <jack86520t@yahoo.com.tw>
2022-08-21 15:39:26 +0800

    fix

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
+// const mongoose = require("mongoose");
+// const cookieSession = require("cookie-session");
 
-const passport = require("passport");
+// const passport = require("passport");
 
-const keys = require("./config/keys");
-require("./models/User");
-require("./models/Survey");
-require("./passport");
+// const keys = require("./config/keys");
+// require("./models/User");
+// require("./models/Survey");
+// require("./passport");
 
-const auth = require("./routes/authRoutes");
-const survey = require("./routes/surveyRoutes");
+// const auth = require("./routes/authRoutes");
+// const survey = require("./routes/surveyRoutes");
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(keys.mongoURI, {
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("Database Connected"))
-  .catch((err) => console.log("ee", err, "eee"));
+// mongoose.Promise = global.Promise;
+// mongoose
+//   .connect(keys.mongoURI, {
+//     useNewUrlParser: true,
+//   })
+//   .then(() => console.log("Database Connected"))
+//   .catch((err) => console.log("ee", err, "eee"));
 
 const app = express();
-app.use(express.json());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-  })
-);
+// app.use(express.json());
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [keys.cookieKey],
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use("/", auth).use("/", survey);
+// app.use("/", auth).use("/", survey);
 app.get("/", (req, res) => {
   res.send("Hello World3!");
 });
