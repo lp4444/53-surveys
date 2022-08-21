d982e0d62d5f30f2fb26db3c2237450a64876419
lp4444 <jack86520t@yahoo.com.tw>
2022-08-21 19:21:00 +0800

    fix2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require("cookie-session");
 
 // const passport = require("passport");
 
-// const keys = require("./config/keys");
+const keys = require("./config/keys");
 // require("./models/User");
 // require("./models/Survey");
 // require("./passport");
@@ -22,12 +22,12 @@ const cookieSession = require("cookie-session");
 
 const app = express();
 // app.use(express.json());
-// app.use(
-//   cookieSession({
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieKey],
-//   })
-// );
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
 // app.use(passport.initialize());
 // app.use(passport.session());
