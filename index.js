fbb8837a8b270fd131dd3456ba4676afbb8aa894
lp4444 <jack86520t@yahoo.com.tw>
2022-08-21 20:06:52 +0800

    fix2

--- index.js
+++ index.js
@@ -42,4 +42,12 @@ app.get("/we", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+// app.listen(PORT);
+
+app.listen(PORT, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
