8d581f8202775aae63bde96cb07518b28303d892
lp4444 <jack86520t@yahoo.com.tw>
2022-08-21 20:17:47 +0800

    fix2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose"); //t
-const cookieSession = require("cookie-session");
+// const mongoose = require("mongoose"); //t
+// const cookieSession = require("cookie-session");
 
 const passport = require("passport");
 
@@ -12,22 +12,22 @@ const keys = require("./config/keys");
 const auth = require("./routes/authRoutes");
 // const survey = require("./routes/surveyRoutes");//t
 
-mongoose.Promise = global.Promise; //t
-mongoose
-  .connect(keys.mongoURI, {
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("Database Connected"))
-  .catch((err) => console.log("ee", err, "eee")); //t
+// mongoose.Promise = global.Promise; //t
+// mongoose
+//   .connect(keys.mongoURI, {
+//     useNewUrlParser: true,
+//   })
+//   .then(() => console.log("Database Connected"))
+//   .catch((err) => console.log("ee", err, "eee")); //t
 
 const app = express();
 app.use(express.json());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-  })
-);
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [keys.cookieKey],
+//   })
+// );
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,12 +42,12 @@ app.get("/we", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT);
+app.listen(PORT);
 
-app.listen(PORT, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+// app.listen(PORT, function () {
+//   console.log(
+//     "Express server listening on port %d in %s mode",
+//     this.address().port,
+//     app.settings.env
+//   );
+// });
