87475cf0aa604acb26513d95c543d604385ece3c
lp4444 <jack86520t@yahoo.com.tw>
2022-08-29 03:11:06 +0800

    fix2

--- client2/src/components/surveys/formFields.js
+++
@@ -1,4 +0,0 @@
-export default [
-  { label: "Campaign Title", name: "title" },
-  { label: "Subject Line", name: "subject" },
-];
--- client2/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
-import surveysReducer from "./surveysReducer";
-import authReducer from "./authReducer";
-export default combineReducers({
-  form: formReducer,
-  surveys: surveysReducer,
-  auth: authReducer,
-});
